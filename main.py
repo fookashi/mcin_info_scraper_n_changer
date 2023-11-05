e14032b28bb2ff2d2e0ae90463687ae4c006f3ca
fookashi <darezzzlol@gmail.com>
2023-11-05 18:20:51 +0300

    Refactored main.py: split change_names function into change_names_in_json and change_names_on_website, added type hints, and updated function calls in main block

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 from src.changer import InfoChanger, ChangeMode
 from src.scraper import AuthorScraper
-import orjson
 
 
-def change_names(email, password):
+def change_names_in_json(email: str, password: str):
     # There are two modes: direct_change and changes_in_json
     # Look at docstring of InfoChanger for more
     changer = InfoChanger(email, password, ChangeMode.changes_in_json)
-    changer.change_info('data/authors.json', max_tabs=10)
+    changer.change_info('data/authors.json')
 
 
-def scrape_names(email, password, symbols):
+def change_names_on_website(email: str, password: str, max_tabs: int = 10):
+    # There are two modes: direct_change and changes_in_json
+    # Look at docstring of InfoChanger for more
+    changer = InfoChanger(email, password, ChangeMode.direct_change)
+    changer.change_info('data/authors.json', max_tabs=max_tabs)
+
+
+def scrape_names(email: str, password: str, symbols: str):
     scraper = AuthorScraper(email, password)
     scraper.scrape_authors_info_into_file(symbols)
 
 
 if __name__ == "__main__":
-    email = 'your email'
-    password = 'your password'
-    change_names(email, password)
+    email = 'email'
+    password = 'password'
+    # firstly!
+    scrape_names(email, password, 'Аб')
+    change_names_in_json(email, password)
+    # after checked changed authors.json that changes of names are correct!
+    ##change_names_on_website(email, password, 15)
+
+
 
